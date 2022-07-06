ac6cc699b9c966e41103bc173f29e1fda3d74fd1
Jamaria Sims <75871993+JamariaSims@users.noreply.github.com>
2022-07-06 14:05:59 -0400

    api route get user by username

--- routes/users.js
+++ routes/users.js
@@ -6,75 +6,85 @@ const User = require("../models/users");
  * CREATE
  */
 rooter.post("/", (req, res) => {
-    const { body } = req;
-    const { username, password } = body;
-    console.log(body);
-    newUser = new User({
-        username: username,
-        password: password,
+  const { body } = req;
+  const { username, password } = body;
+  console.log(body);
+  newUser = new User({
+    username: username,
+    password: password,
+  });
+  newUser
+    .save()
+    .then((newUser) => {
+      res.send(newUser);
+    })
+    .catch((err) => {
+      if (err.code === 11000) {
+        res.status(400).send("Username Taken!");
+      }
+      res.status(400).send(err);
     });
-    newUser
-        .save()
-        .then((newUser) => {
-            res.send(newUser);
-        })
-        .catch((err) => {
-            if (err.code === 11000) {
-                res.status(400).send("Username Taken!");
-            }
-            res.status(400).send(err);
-        });
 });
 /**
  * READ
  */
 rooter.get("/", (req, res) => {
-    User.find()
-        .then((User) => {
-            res.send(User);
-        })
-        .catch((err) => {
-            res.send(err);
-        });
+  User.find()
+    .then((User) => {
+      res.send(User);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 });
-rooter.get("/:userId", (req, res) => {
-    User.findById(req.params.userId)
-        .then((user) => {
-            if (!user) {
-                res.status(404).send("User not found!");
-            }
-            res.send(user);
-        })
-        .catch((err) => {
-            res.send(err);
-        });
+// rooter.get("/:userId", (req, res) => {
+//   User.findById(req.params.userId)
+//     .then((user) => {
+//       if (!user) {
+//         res.status(404).send("User not found!");
+//       }
+//       res.send(user);
+//     })
+//     .catch((err) => {
+//       res.send(err);
+//     });
+// });
+rooter.get("/:username", (req, res, next) => {
+  User.findOne({ username: req.params.username })
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      res.status(404).send("User not found!");
+    });
 });
+
 /**
  * UPDATE
  */
 rooter.put("/:userId", async (req, res) => {
-    const updatedUser = await User.findByIdAndUpdate(
-        req.params.userId,
-        {
-            username: req.body.username,
-        },
-        { new: true }
-    );
+  const updatedUser = await User.findByIdAndUpdate(
+    req.params.userId,
+    {
+      username: req.body.username,
+    },
+    { new: true }
+  );
 
-    if (!updatedUser) {
-        res.status(404).send("User not found!");
-    }
-    res.send(updatedUser);
+  if (!updatedUser) {
+    res.status(404).send("User not found!");
+  }
+  res.send(updatedUser);
 });
 /**
  * DELETE
  */
 rooter.delete("/:userId", async (req, res) => {
-    const newUser = await User.findByIdAndRemove(req.params.userId);
-    if (!newUser) {
-        res.status(404).send("User not found!");
-    }
-    res.send(newUser);
+  const newUser = await User.findByIdAndRemove(req.params.userId);
+  if (!newUser) {
+    res.status(404).send("User not found!");
+  }
+  res.send(newUser);
 });
 
 module.exports = rooter;
