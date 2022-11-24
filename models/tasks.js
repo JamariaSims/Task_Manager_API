dbbe6da6f35ddc34ef081b11063307c202d950aa
JamariaSims <jamariaxsims@gmail.com>
2022-11-23 20:40:50 -0500

    Update

--- models/tasks.js
+++ models/tasks.js
@@ -3,49 +3,49 @@ const mongoose = require("mongoose");
 //TASK SCHEMA
 
 const TaskSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        min_length: 8,
-        max_length: 50,
-        unique: true,
-    },
-    description: {
-        type: String,
-        required: true,
-        min_length: 8,
-        max_length: 255,
-    },
-    deadline: {
-        type: String,
-        required: true,
-        min_length: 6,
-        max_length: 10,
-    },
-    priority: {
-        type: String,
-        required: true,
-        min_length: 4,
-        max_length: 20,
-    },
-    status: {
-        type: String,
-        required: true,
-        min_length: 4,
-        max_length: 12,
-    },
-    group_name: {
-        type: String,
-        required: true,
-        min_length: 8,
-        max_length: 50,
-    },
-    created_by: {
-        type: String,
-        required: true,
-        min_length: 8,
-        max_length: 50,
-    },
+  name: {
+    type: String,
+    required: true,
+    min_length: 8,
+    max_length: 50,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    min_length: 8,
+    max_length: 255,
+  },
+  deadline: {
+    type: String,
+    required: true,
+    min_length: 6,
+    max_length: 10,
+  },
+  priority: {
+    type: String,
+    required: true,
+    min_length: 4,
+    max_length: 20,
+  },
+  status: {
+    type: String,
+    required: true,
+    min_length: 4,
+    max_length: 12,
+  },
+  group_name: {
+    type: String,
+    required: true,
+    min_length: 0,
+    max_length: 50,
+  },
+  created_by: {
+    type: String,
+    required: true,
+    min_length: 0,
+    max_length: 50,
+  },
 });
 
 module.exports = new mongoose.model("Task", TaskSchema);
